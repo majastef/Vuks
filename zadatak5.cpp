8ac434c036ea1844a71ab1fbdca36748a06f1dde
Maja Stefanović <115740512+majastef@users.noreply.github.com>
2022-11-11 16:37:04 +0100

    Create zadatak5.cpp

---
+++ zadatak5.cpp
@@ -0,0 +1,10 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define SUCCESS 0
+#define MAX_FILE_NAME 50
+#define MAX_LINE 1024
+#define FILE_DIDNT_OPEN_ERROR -1
+#define ERROR -2
